51e1f4cfb495a7dbd8ae2f4efde4d0cf189e5131
Rafael <rafael.pascale@zasso.com>
2022-06-09 21:18:49 -0300

    add files from unisal

--- SequenciaAmostraUnitária.m/impseq.m
+++ SequenciaAmostraUnitária.m/impseq.m
@@ -1,11 +1,10 @@
-function [x,n]=impseq(n0,n1,n2)
+[x,n]=impseq(n0,n1,n2);
 %---------------------------------------
 % Gera x(n) = delta(n-n0); n1<= n <= n2
 %---------------------------------------
 % [x,n] = impseq(n0, n1, n2)
 %
-n=[n1:n2];
-x=[(n-n0)==0];
+n=[n1:n2]; x=[(n-n0)==0];
 stem(n,x);
 title('Funcao Impulso Delta(n-n0)');
 xlabel('amostra [n]');
